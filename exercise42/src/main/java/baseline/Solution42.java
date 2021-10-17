892ff71b353263b5a9541d1f22865532bb2edb99
Nick <lopes.nick0801@gmail.com>
2021-10-16 20:39:04 -0400

    Skeleton Code

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Nicholas Lopes
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution42 {
+    public static void main(String[] args){
+        //initialize starting variables
+        //load files contents into string
+        //pass string to split method which returns string array
+        //display parsed data
+    }
+}
