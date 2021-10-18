3c87252295c3bccc0a8f13bf7d6bfcaa9758dcf8
Nick <lopes.nick0801@gmail.com>
2021-10-18 11:07:39 -0400

    Solution43

--- exercise43/.idea/sonarlint/issuestore/index.pb
+++ exercise43/.idea/sonarlint/issuestore/index.pb
@@ -2,10 +2,10 @@
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
-<
-build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
 G
 data/exercise43UML.puml,7\7\7767df2c39c9d2333e7027693484bc57e40299ef
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
 V
 &src/main/java/baseline/Solution43.java,b\4\b4347542199e6f8f4a0c2a06abf7605a025d7459
 Z
--- exercise43/src/test/java/baseline/Solution43Test.java
+++ exercise43/src/test/java/baseline/Solution43Test.java
@@ -2,11 +2,28 @@ package baseline;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution43Test {
     @Test
-    void testMain() {
+    void testMain() throws IOException {
+        boolean expected = true;
+        String name = "webtest";
+        String author = "tester";
+        Solution43 test = new Solution43();
+        String path = "data/test/";
+
+        test.makeDir(path, name, author);
+
+        Path dir = Paths.get(path + name);
+        boolean actual = Files.exists(dir);
+
+        assertEquals(expected, actual);
 
     }
 }
